ce03615224ebdc09c9db84d9864e7badf9749459
Sing Tuttanon <singtuttanon@gmail.com>
2023-10-13 11:15:13 -0400

    Roll The Dice Completed

--- src/main/java/com/pluralsight/RollTheDice.java
+++ src/main/java/com/pluralsight/RollTheDice.java
@@ -6,12 +6,12 @@ public class RollTheDice {
         Dice dice = new Dice();
 //  Declare variables
         int roll1, roll2, twoCounter = 0, fourCounter = 0, sixCounter = 0, sevenCounter = 0;
-//  For Loop (Roll 20x)
-        for (int i = 0; i < 20; i++) {
+//  For Loop (Repeat 100x)
+        for (int i = 0; i <= 100; i++) {
             roll1 = dice.roll(); // Dice 1
             roll2 = dice.roll(); // Dice 2
 //  Display outputs from each dice roll and sum
-            System.out.println("Roll 1: " + roll1 + " - Roll 2: " + roll2 + " Sum: " + (roll1 + roll2));
+            System.out.println("Roll 1: " + roll1 + " | Roll 2: " + roll2 + " Sum: " + (roll1 + roll2));
 //  Sum equation input
             int sum = roll1 + roll2;
 //  If/else-if Statements (Four counters)
